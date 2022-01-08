cd04782feb865d6ee7121af9de6098ea49009bca
Andrii Divnych <divnycha05@gmail.com>
2022-01-08 14:39:49 +0200

    First section is done, work with components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -22,8 +23,6 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-
-
   return (
     <div>
       <h2>Let's get started!</h2>
@@ -31,5 +30,14 @@ function App() {
     </div>
   );
 }
+
+  // return React.createElement(
+  //   'div', 
+  //   {}, 
+  //   React.createElement('h2', {}, 'Let\'s get started'), 
+  //   React.createElement(Expenses, {items: expenses})
+  //   )
+  // };
+
 
 export default App;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
-    <div className = 'expenses'>
+    <Card className = 'expenses'>
       <ExpenseItem
         title={props.items[0].title}
         date={props.items[0].date}
@@ -19,7 +20,7 @@ function Expenses(props) {
         date={props.items[2].date}
         amount={props.items[2].amount}
       />
-    </div>
+    </Card>
   );
 }
 
